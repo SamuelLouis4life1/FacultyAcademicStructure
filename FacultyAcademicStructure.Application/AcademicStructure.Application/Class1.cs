bee9ffd021fd220d7b44a4bb16ed5a90adf63223
Samuel Louis Sampeur <sampeur4life1@gmail.com>
2021-11-18 20:02:28 -0800

    Add project files.

---
+++ FacultyAcademicStructure.Application/AcademicStructure.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AcademicStructure.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ FacultyAcademicStructure.Infrastructure/AcademicStructure.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AcademicStructure.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
