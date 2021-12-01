cc92d6729a0412a76e0c566aa1f367e184b6f0df
Samuel Louis Sampeur <sampeur4life1@gmail.com>
2021-12-01 13:07:27 -0800

    Infrastructure Service Registrations Class, Applying Clean Architecture

--- FacultyAcademicStructure.Api/AcademicStructure.Api/Startup.cs
+++ FacultyAcademicStructure.Api/AcademicStructure.Api/Startup.cs
@@ -25,7 +25,8 @@ namespace AcademicStructure.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddApplicationSeservices();
+            // services.AddApplicationSeservices();
+            // services.InfrastructureServiceRegistration.cs
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
---
+++ FacultyAcademicStructure.Infrastructure/AcademicStructure.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,32 @@
+﻿using AcademicStructure.Application.Contracts.Infrastructure;
+using AcademicStructure.Application.Contracts.Persistence;
+using AcademicStructure.Application.Models;
+using AcademicStructure.Infrastructure.Mail;
+using AcademicStructure.Infrastructure.Persistence;
+using AcademicStructure.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace AcademicStructure.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDataBContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("ApplicationConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<ITeacherRepository, TeacherRepository>();
+            services.AddScoped<IStudentRepository, StudentRepository>();
+            services.AddScoped<IClassRoomRepository, ClassRoomRepository>();
+
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
