8f721bd9760df871b9a9cb65e10480bef01581d0
Samuel Louis Sampeur <sampeur4life1@gmail.com>
2021-12-01 07:14:26 -0800

    Developing Student.API and Teacher.API for presentation Layer in Clean Architecture.

---
+++ FacultyAcademicStructure.Api/AcademicStructure.Api/Controllers/StudentController.cs
@@ -0,0 +1,70 @@
+﻿using AcademicStructure.Application.Features.Students.Commands.CheckoutStudent;
+using AcademicStructure.Application.Features.Students.Commands.DeleteStudent;
+using AcademicStructure.Application.Features.Students.Commands.UpdateStudent;
+using AcademicStructure.Application.Features.Students.Queries.GetTeachersList;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace AcademicStructure.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StudentController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+
+        [HttpGet("{studentName}", Name = "GetStudent")]
+        [ProducesResponseType(typeof(IEnumerable<StudentVm>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<StudentVm>>> GetStudentsByName(string studentName)
+        {
+            var query = new GetStudentListQuery(studentName);
+            var students = await _mediator.Send(query);
+            return Ok(students);
+        }
+
+
+        [HttpPost(Name = "Students")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<int>> Students ([FromBody] StudentCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+
+        [HttpPut(Name = "UpdateStudent")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> UpdateStudent([FromBody] UpdateStudentCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id}", Name = "DeleteStudent")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> DeleteStudent(int id)
+        {
+            var command = new DeleteStudentCommand() { Id = id };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+    }
+}
---
+++ FacultyAcademicStructure.Api/AcademicStructure.Api/Controllers/TeacherController.cs
@@ -0,0 +1,70 @@
+﻿using AcademicStructure.Application.Features.Teachers.Commands.CheckoutTeacher;
+using AcademicStructure.Application.Features.Teachers.Commands.DeleteTeacher;
+using AcademicStructure.Application.Features.Teachers.Commands.UpdateTeacher;
+using AcademicStructure.Application.Features.Teachers.Queries.GetTeachersList;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace AcademicStructure.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class TeacherController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TeacherController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+
+        [HttpGet("{teacherName}", Name = "GetTeacher")]
+        [ProducesResponseType(typeof(IEnumerable<TeacherVm>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<TeacherVm>>> GetTeacherByName(string teacherName)
+        {
+            var query = new GetTeacherListQuery(teacherName);
+            var teachers = await _mediator.Send(query);
+            return Ok(teachers);
+        }
+
+
+        [HttpPost(Name = "ReadTeachers")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<int>> ReadTeachers([FromBody] TeacherCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+
+        [HttpPut(Name = "UpdateTeacher")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> UpdateTeacher([FromBody] UpdateTeacherCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id}", Name = "DeleteTeacher")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> DeleteTeacher(int id)
+        {
+            var command = new DeleteTeacherCommand() { Id = id };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+    }
+}
