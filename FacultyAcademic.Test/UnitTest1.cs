66024ff4eb58c0ef47958926d9565d94dae96c0b
Samuel Louis Sampeur <sampeur4life1@gmail.com>
2021-12-01 19:59:53 -0800

    Created Test Project

---
+++ FacultyAcademic.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework.Internal;
+
+namespace FacultyAcademic.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
